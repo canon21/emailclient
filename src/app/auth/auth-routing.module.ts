ae15e37a2f342aef78403a539a6ef118e482a10c
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-19 15:44:20 +0200

    Adding routing rule to Auth Module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+import { SignoutComponent } from './signout/signout.component';
+
+//Adding routing rules.
+const routes: Routes = [
+  {path:"signup", component: SignoutComponent},
+  {path:"", component: SigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
