24b9a40c11164a2c5337f97997d97ccd3b39ac28
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-19 16:29:11 +0200

    Adding Custom Validation for match password

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
 
 @Component({
   selector: 'app-signout',
@@ -9,12 +10,15 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SignoutComponent implements OnInit {
 
   authForm = new FormGroup({
-    username: new FormControl('', [Validators.required,Validators.minLength(3),Validators.maxLength(20), Validators.pattern(/^[a-z0-9]+$/)]),
-    password: new FormControl('',[Validators.required,Validators.minLength(4), Validators.maxLength(20)]),
-    passwordConfirmation: new FormControl('',[Validators.required, Validators.minLength(4), Validators.maxLength(20)])
-  });
+    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern(/^[a-z0-9]+$/)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+    passwordConfirmation: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)])
+  },
+  //secondo argomento del formGroup
+  { validators: [this.matchPassword.validate]}
+  );
 
-  constructor() { }
+  constructor(private matchPassword: MatchPassword) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/auth/validators/match-password.ts
@@ -0,0 +1,21 @@
+import { FormGroup, ValidationErrors, Validator } from "@angular/forms";
+import { Injectable } from "@angular/core";
+
+//Attivando la dipendency Injection
+@Injectable({
+    providedIn:'root'
+})
+
+export class MatchPassword implements Validator{
+
+    validate(formGroup : FormGroup): ValidationErrors {
+
+        const {password, passwordConfirmation} = formGroup.value;
+
+        if(password === passwordConfirmation) {
+            return null;
+        }
+        
+        return {passwordDontMatch:true};
+    }
+}
