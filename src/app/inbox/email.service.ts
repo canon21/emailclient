06cec86a5c870a3a72ce7680b5cbf98e60390540
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-07-17 15:25:16 +0200

    Implementation of emailIndexList

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+interface EmailSummary {
+  id: string,
+  subject: string,
+  from: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  basePath: string = 'https://api.angular-email.com';
+
+  constructor(private http: HttpClient) { }
+
+  getEmails() {
+    return this.http.get<EmailSummary[]>(`${this.basePath}/emails`, {
+      //adding cookies
+      withCredentials:true
+    });
+  }
+
 }
