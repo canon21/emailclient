b5a639a999e9605373c38f7afc195bd09d21cda5
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-07-18 19:27:17 +0200

    Implemetion notfOUND EMAIL

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { Email } from './email';
 import { EmailService } from './email.service';
+import { catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailResolverService implements Resolve<Email>{
 
-  constructor(private emailService: EmailService ) { }
+  constructor(private emailService: EmailService, private router: Router ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Email | Observable<Email> | Promise<Email> {
 
     const { id } = route.params;
-    return this.emailService.getEmailById(id);
+    return this.emailService.getEmailById(id)
+      .pipe(
+        catchError( () => {
+          this.router.navigateByUrl('/inbox/not-found');
+          return EMPTY;
+        })
+      );
   }
 
 }
